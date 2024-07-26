0726a8e30e14f74401951d6d89ade30a4c0f62c2
Indu Prakash <leolite1@gmail.com>
2024-07-26 17:15:42 -0500

    Remove inapplicable sensor entities

--- custom_components/weatherapi/__init__.py
+++ custom_components/weatherapi/__init__.py
@@ -1,6 +1,7 @@
 """The WeatherAPI integration."""
 
 from datetime import timedelta
+import logging
 from typing import Final
 
 from homeassistant.config_entries import ConfigEntry
@@ -12,18 +13,23 @@ from homeassistant.const import (
     Platform,
 )
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 
 from .const import (
+    CONFIG_ADD_SENSORS,
     CONFIG_FORECAST,
     CONFIG_IGNORE_PAST_HOUR,
+    DEFAULT_ADD_SENSORS,
     DEFAULT_FORECAST,
     DEFAULT_IGNORE_PAST_HOUR,
     DOMAIN,
     UPDATE_INTERVAL_MINUTES,
 )
 from .coordinator import WeatherAPIUpdateCoordinator, WeatherAPIUpdateCoordinatorConfig
+from .sensor import SENSOR_DESCRIPTIONS
 
 PLATFORMS: Final = [Platform.WEATHER, Platform.SENSOR]
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
@@ -33,11 +39,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     latitude = entry.data[CONF_LATITUDE]
     longitude = entry.data[CONF_LONGITUDE]
+    location_name = entry.data[CONF_NAME]
 
     config = WeatherAPIUpdateCoordinatorConfig(
         api_key=entry.data[CONF_API_KEY],
         location=f"{latitude},{longitude}",
-        name=entry.data[CONF_NAME],
+        name=location_name,
         update_interval=timedelta(minutes=UPDATE_INTERVAL_MINUTES),
         forecast=entry.options.get(CONFIG_FORECAST, DEFAULT_FORECAST),
         ignore_past_forecast=entry.options.get(
@@ -51,6 +58,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    # Remove previous sensor entries if sensors are not being added
+    if not entry.options.get(CONFIG_ADD_SENSORS, DEFAULT_ADD_SENSORS):
+        ent_reg = er.async_get(hass)
+
+        for description in SENSOR_DESCRIPTIONS:
+            unique_id = coordinator.generate_sensor_unique_id(description)
+
+            if entity_id := ent_reg.async_get_entity_id(
+                Platform.SENSOR, DOMAIN, unique_id
+            ):
+                _LOGGER.debug("Removing sensor entity %s", entity_id)
+                ent_reg.async_remove(entity_id)
 
     return True
 
